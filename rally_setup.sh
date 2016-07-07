c16493b1034ff2643623279e8a8d13adbd6fa5ee
Abhishek Shrivastava <AbhiCloudbyte@users.noreply.github.com>
2016-07-07 12:00:43 +0530

    Update rally_setup.sh

--- rally_setup.sh
+++ rally_setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 git clone https://git.openstack.org/openstack-dev/devstack
-git clone https://github.com/openstack/rally.git
-cp rally/contrib/devstack/lib/rally devstack/lib/
-cp rally/contrib/devstack/extras.d/70-rally.sh devstack/extras.d/
 cp local.conf devstack/
-rm -rf rally
 cd devstack/ && ./stack.sh
